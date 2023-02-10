eecc333edb8d422f24dc5246ba2e16fcd3fc68dc
Bruno Rodrigues <bruno.brd73@gmail.com>
2023-02-09 23:19:20 -0300

    refactor: modified exercices

--- build/exercicio4.js
+++ build/exercicio4.js
@@ -3,10 +3,10 @@
 Leia um vetor de 20 posições e em seguida um valor X qualquer.
 Seu programa devera fazer uma busca do valor de X no vetor lido e
 informar a posição em que foi encontrado ou se não foi encontrado. */
-//                       0  1  2  3  4  5  6  7  8   9...
-// const arrr: number[] = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-const arrr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-const indexRandomOfArray = Math.floor(Math.random() * arrr.length);
-console.log(indexRandomOfArray);
-const resultValue = arrr.find(indexRandomOfArray);
-console.log(`valor: ${resultValue}, índice: ${indexRandomOfArray}`);
+const arrr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+function valueAndIndexOfArray(arrr) {
+    const indexRandomOfArray = Math.floor(Math.random() * arrr.length);
+    const element = arrr[indexRandomOfArray];
+    return `índice: ${indexRandomOfArray} e valor: ${element}`;
+}
+console.log(`${valueAndIndexOfArray(arrr)}`);
--- src/exercicio5.ts
+++ src/exercicio5.ts
@@ -1,3 +1,17 @@
 /* Leia um vetor de 40 posições. Contar 
 e escrever quantos valores pares ele possui. */
 
+const arrayEx5: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function pairValue() {
+    let pairs = 0;
+    for (let index = 0; index < arrayEx5.length; index++) {
+        if (arrayEx5[index] % 2 == 0) {
+            console.log(`${arrayEx5[index]}`)
+            pairs++;
+        } 
+    }
+    console.log(`Números pares: ${pairs}`);
+}
+
+console.log(pairValue())
